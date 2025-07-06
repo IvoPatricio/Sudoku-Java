66a83139811bffcd7e0ecb4e93e32cd2f5521935
IvoPatricio <snoukefm@gmail.com>
2025-07-06 17:33:56 +0100

    Create file: main sudoku execution

---
+++ Sudoku.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Sudoku {
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Sudoku sudokuGame = new Sudoku();
+        boolean isrunning = true;
+        while (isrunning)
+        {
+            ;
+        }
+        sudokuGame.scanner.close();
+    }
+}
