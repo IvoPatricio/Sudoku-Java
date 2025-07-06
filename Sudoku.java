bb10152a55de84a365073173588ca4a0fb5a2966
IvoPatricio <snoukefm@gmail.com>
2025-07-06 19:49:26 +0100

    Update ft_userInputMenuChoice function

--- Sudoku.java
+++ Sudoku.java
@@ -60,7 +60,21 @@ public class Sudoku {
         {
             case 0:
                 return false;
-            default:
+            case 1:
+                return true;
+            case 2:
+                return true;
+            case 3:
+                return true;
+            case 4:
+                return true;
+            case 5:
+                return true;
+            case 6:
+                return true;
+            case 7:
+                return true;
+            case 8:
                 return true;
         }
     }
